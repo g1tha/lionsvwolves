7e52b8e5713dfed963fdb34826d015a5c01bebed
g1tha <jeewantha.k@gmail.com>
2022-05-04 12:47:29 +1000

    Completed cs50x homepage pset.

---
+++ README.md
@@ -0,0 +1,34 @@
+This is an exercise for a cs50x pset.
+Further information here: https://cs50.harvard.edu/x/2022/psets/8/homepage/.
+
+Sources:
+
+Lion 1:
+Photo by Pixabay: https://www.pexels.com/photo/close-up-portrait-of-lion-247502/
+
+Lion 2:
+Photo by Alexas Fotos: https://www.pexels.com/photo/close-up-photo-of-lion-s-head-2220336/
+
+Lion 3:
+Photo by Pixabay: https://www.pexels.com/photo/close-up-photography-of-brown-lion-33045/
+
+Lion 4:
+Photo by Alexas Fotos: https://www.pexels.com/photo/brown-lion-2220337/
+
+Lion 5:
+Photo by Ralph: https://www.pexels.com/photo/brown-lion-2270848/
+
+Wolf 1:
+Photo by Steve: https://www.pexels.com/photo/brown-wolf-682375/
+
+Wolf 2:
+Photo by Pixabay: https://www.pexels.com/photo/gray-and-white-wolf-39310/
+
+Wolf 3:
+Photo by Anna Hinckel: https://www.pexels.com/photo/brown-wolf-on-green-grass-5873592/
+
+Wolf 4:
+Photo by Nicky Pe: https://www.pexels.com/photo/wood-animal-dog-cute-7964610/
+
+Wolf 5:
+Photo by Steve: https://www.pexels.com/photo/wolf-standing-on-stone-397863/ 
---
+++ scripts.js
@@ -0,0 +1,103 @@
+const htmlEl = document.getElementsByTagName('html')[0];
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.nav-toggle');
+const navBurger = document.querySelector('.bi-list');
+const navClose = document.querySelector('.bi-x');
+const loginLink = document.querySelector('.login-link');
+const loginText = loginLink.querySelector('.login-text');
+const arrowRight = loginLink.querySelector('.bi-box-arrow-in-right');
+const arrowLeft = loginLink.querySelector('.bi-box-arrow-left');
+const themeLink =document.querySelector('.theme-link');
+const lightIcon = themeLink.querySelector('.bi-sun-fill');
+const darkIcon = themeLink.querySelector('.bi-moon-fill');
+const loginBtn = document.querySelector('.btn-login');
+const loginLbl = document.querySelector('.login-lbl');
+const themeSwitch = document.querySelector('.theme-switch');
+const themeLbl = document.querySelector('.theme-lbl');
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+    if (visibility === 'false') {
+        primaryNav.setAttribute('data-visible', 'true');
+        navToggle.setAttribute('aria-expanded', 'true');
+        navBurger.setAttribute('data-visible', 'false');
+        navClose.setAttribute('data-visible', 'true');
+    } else {
+        primaryNav.setAttribute('data-visible', 'false');
+        navToggle.setAttribute('aria-expanded', 'false');
+        navBurger.setAttribute('data-visible', 'true');
+        navClose.setAttribute('data-visible', 'false');
+    }
+})
+
+loginLink.addEventListener('click', loginFunc);
+if (loginBtn !== null) {
+    loginBtn.addEventListener('click', loginFunc);
+}
+
+function loginFunc() {
+    const visibility = arrowRight.getAttribute('data-visible');
+    if (visibility === 'false') {
+        arrowLeft.setAttribute('data-visible', 'false');
+        arrowRight.setAttribute('data-visible', 'true');
+        loginText.textContent = "Log in";
+        if (loginBtn !== null) {
+            loginBtn.textContent = "Login";
+            loginLbl.textContent = "Log in";
+        }
+        
+    } else {
+        arrowLeft.setAttribute('data-visible', 'true');
+        arrowRight.setAttribute('data-visible', 'false');
+        loginText.textContent = "Log out";
+        if (loginBtn !== null) {
+            loginBtn.textContent = "Logout";
+            loginLbl.textContent = "Log out";
+        }
+    }
+}
+
+let lightMode = localStorage.getItem('lightMode');
+themeLink.addEventListener('click', changeTheme);
+if (themeSwitch !== null){
+    themeSwitch.addEventListener('click', changeTheme);
+}
+
+
+if (lightMode === 'enabled') {
+    enableLightMode();
+} else {
+    disableLightMode();
+}
+
+function changeTheme() {
+    if (lightMode === 'enabled') {
+        disableLightMode();
+    } else {
+        enableLightMode();
+    }
+}
+
+function enableLightMode() {
+    darkIcon.setAttribute('data-visible', 'true');
+    lightIcon.setAttribute('data-visible', 'false');
+    localStorage.setItem('lightMode', 'enabled');
+    lightMode = localStorage.getItem('lightMode');
+    htmlEl.setAttribute('data-theme', 'light');
+    if (themeSwitch !== null) {
+        themeLbl.textContent = "Light theme on";
+        themeSwitch.checked = true;
+    }
+}
+
+function disableLightMode() {
+    darkIcon.setAttribute('data-visible', 'false');
+    lightIcon.setAttribute('data-visible', 'true');
+    localStorage.setItem('lightMode', null);
+    lightMode = localStorage.getItem('lightMode');
+    htmlEl.setAttribute('data-theme', 'dark');
+    if (themeSwitch !== null) {
+        themeLbl.textContent = "Light theme off";
+        themeSwitch.checked = false;
+    } 
+}
